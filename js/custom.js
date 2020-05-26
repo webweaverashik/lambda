f160a71dbc0ec5b265c01ddc28073f72e4c25355
Ashikur Rahman <ashikgsc170@gmail.com>
2020-05-26 20:47:30 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -21,3 +21,15 @@ function closeNav(){
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector("#today").value = today;
 
+
+
+
+
+	// One Page Nav
+	var top_offset = $('.header-area').height() - 650;
+	$('.header-area nav ul').onePageNav({
+		currentClass: 'active',
+		scrollOffset: top_offset,
+	});
+
+
